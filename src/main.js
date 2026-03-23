9c27de207b5df4b69af37f00062caeb61ebb74c5
HappyNoose <78141897+MrZahourek@users.noreply.github.com>
2026-03-23 17:17:50 +0100

    idk why this isnt workinh

--- src/main.js
+++ src/main.js
@@ -1,10 +1,34 @@
 import './style.css'
 import Phaser from 'phaser'
 
+const sizes = {
+    width: 500,
+    height: 500
+};
+
 const config = {
     type: Phaser.WEBGL,
-    width: 500,
-    height: 500,
+    width: sizes.width,
+    height: sizes.height,
     canvas: gameCanvas
 };
 
+const game = new Phaser.Game(config);
+
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super("scene-game");
+    }
+
+    preload() {
+        this.load.image("bg", "/assets/back_1.png");
+        this.load.image("enemy_1", "/assets/visitor_1.png");
+    }
+
+    create() {
+        this.add.image(0, 0, "bg").setOrigin(0, 0);
+        this.add.image(0, 0, "enemy_1").setOrigin(0, 0);
+    }
+
+    update() {}
+}
